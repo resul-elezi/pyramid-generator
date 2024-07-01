e376cf9f96d4ccd4365cc4c366e8e841021d8c1f
Resul Elezi <resul_elezi@protonmail.com>
2024-07-01 12:05:08 +0200

    #

--- js/script.js
+++ js/script.js
@@ -10,7 +10,17 @@ function padRow(rowNumber, rowCount) {
 // for (let i = 1; i <= count; i++) {
 //   rows.push(padRow(i, count));
 // }
+
+let continueLoop = false;
+let done = 0;
+
+while (continueLoop) {
+  done++;
   
+  if (done === count) {
+
+  }
+}
   let result = ""
   
   for (const row of rows) {
